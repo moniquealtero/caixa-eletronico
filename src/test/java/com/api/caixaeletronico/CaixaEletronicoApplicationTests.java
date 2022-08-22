0af640dc5fbe5f732ff6c0efdddfa23e44e69ea2
nadia-alexandrino <83779166+nadia-alexandrino@users.noreply.github.com>
2022-08-22 18:57:45 -0300

    Add files via upload

---
+++ src/test/java/com/api/caixaeletronico/CaixaEletronicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.caixaeletronico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaixaEletronicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
